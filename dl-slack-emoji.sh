6709d24afaeeefd463074edb8c5973ecd7e687d8
Thaddee Tyl <thaddee.tyl@gmail.com>
2016-06-28 17:22:36 +0200

    Usage

--- dl-slack-emoji.sh
+++ dl-slack-emoji.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+function usage {
+  cat <<USAGE
+Usage: dl-slack-emoji.sh project token
+
+  project: https://<project>.slack.com
+  token: get one at <https://api.slack.com/docs/oauth-test-tokens>.
+USAGE
+}
+
+if [ "$1" == "" ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+  usage
+  exit
+fi
+
 # https://<project>.slack.com
 project=$1
 # Get a OAuth test token at <https://api.slack.com/docs/oauth-test-tokens>.
 token=$2
 
-getlinks() {
+function getlinks {
   curl "https://$project.slack.com/api/emoji.list" -d token="$token" |
   jq .emoji[] |
   grep https |
